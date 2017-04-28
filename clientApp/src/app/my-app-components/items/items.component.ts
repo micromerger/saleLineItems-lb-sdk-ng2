139f33c6f7adb677720b65718013b96a32953855
jibransiddiqui <jabran.bsse2074@iiu.edu.pk>
2017-04-28 15:39:16 +0500

    itemAdd

--- clientApp/src/app/my-app-components/items/items.component.ts
+++ clientApp/src/app/my-app-components/items/items.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Items, FireLoopRef } from 'app/shared/sdk/models';
+import { RealTime } from 'app/shared/sdk/services';
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
+  }
+  private itemedit : boolean = false;
+  private item     : Items = new Items();
+  private reference : FireLoopRef<Items>;
+  private items     : Items[] = new Array<Items>();
+  constructor(private rt: RealTime) {
+     this.rt.onReady().subscribe(() => {
+    this.reference = this.rt.FireLoop.ref<Items>(Items);
+    this.reference.on('change').subscribe(( items : Items[]) => this.items = items);
+     });
+  }
+cancel() : void{
+    this.itemedit = false;
+    this.item.name=null;
+    this.item.price=null;
+  }
+  itemSubmitRecord() : void{
+    this.reference.upsert(this.item).subscribe();
+    this.itemedit = false;
+    this.item.name=null;
+    this.item.price=null;
+  }
+  editItem(id) : void{
+     this.itemedit = true;
+    for (var i = 0; i<this.items.length;i++){
+      if (this.items[i].id  == id)
+      this.item = this.items[i];
+    }
+  }
+  deleteItem(item : Items) :void{
+     this.reference.remove(item).subscribe();
+  }
+  add(): void {
+    this.reference.create(this.item).subscribe(() => this.item = new Items());
+  }
+
+  update(item: Items): void {
+    this.reference.upsert(item).subscribe();
+  }
+
+  remove(item: Items): void {
+    this.reference.remove(item).subscribe();
   }
 
 }
